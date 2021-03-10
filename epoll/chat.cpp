732ba2a1bb7855d35cea85892fe1581fef198d40
LoveBettygirl <bettycyygirl@163.com>
2021-03-10 12:17:09 +0800

    Add epoll main program

---
+++ epoll/chat.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cstring>
+#include <regex>
+#include <getopt.h>
+using namespace std;
+
+void showVersion()
+{
+    cout << "v1.0.0" << endl;
+    exit(0);
+}
+
+void showUsage()
+{
+    cout << "Options:" << endl;
+    cout << " -o, --option               Run as server(s) or client(c) mode. (Required)" << endl;
+    cout << " -a, --address=IP_ADDRESS   The IP address of the server." << endl;
+    cout << " -p, --port=PORT            The port of the server." << endl;
+    cout << " -v, --version              Print the version number and exit." << endl;
+    cout << " -h, --help                 Print this message and exit." << endl;
+    exit(1);
+}
+
+int main(int argc, char **argv)
+{
+    int c;
+    bool client = false;
+    int port = 2000;
+    string ip = "0.0.0.0";
+    regex r("^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)$");
+
+    if (argc == 1)
+        showUsage();
+
+    while (true)
+    {
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"option", required_argument, 0, 'o'},
+            {"address", required_argument, 0, 'a'},
+            {"port", required_argument, 0, 'p'},
+            {"version", no_argument, 0, 'v'},
+            {"help", no_argument, 0, 'h'},
+            {0, 0, 0, 0}};
+
+        c = getopt_long(argc, argv, "o:a:p:vh",
+                        long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'o':
+            if (strcmp(optarg, "s") == 0)
+                client = false;
+            else if (strcmp(optarg, "c") == 0)
+                client = true;
+            else
+            {
+                cerr << "The option value must be \"s\" or \"c\"." << endl;
+                exit(-1);
+            }
+            break;
+
+        case 'a':
+            ip = string(optarg);
+            if (!regex_match(ip, r)) {
+                cerr << "Invaild IP address format." << endl;
+                exit(-1);
+            }
+            break;
+
+        case 'p':
+            port = atoi(optarg);
+            if (port < 0 || port > 65535) {
+                cerr << "The port " << port << " out of range." << endl;
+                exit(-1);
+            }
+            break;
+
+        case 'v':
+            showVersion();
+            break;
+
+        case 'h':
+        case '?':
+            showUsage();
+            break;
+
+        default:
+            showUsage();
+        }
+    }
+
+    if (optind < argc)
+        showUsage();
+
+    if (client) {
+
+    }
+    else {
+
+    }
+
+    exit(EXIT_SUCCESS);
+}
